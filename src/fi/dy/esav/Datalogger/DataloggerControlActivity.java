f8281d314c309ed7937a9ae75c97ed46cc64f8a4
Esa Varemo <esa@kuivanto.fi>
2012-03-16 14:22:45 +0200

    init

---
+++ src/fi/dy/esav/Datalogger/DataloggerControlActivity.java
@@ -0,0 +1,80 @@
+package fi.dy.esav.Datalogger;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import fi.dy.esav.Datalogger.R;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DataloggerControlActivity extends Activity {
+	
+	TextView statusLabel;
+	
+
+    public int onStartCommand() {
+        setContentView(R.layout.control);
+        statusLabel = (TextView) findViewById(R.id.statusLabel);
+        
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(
+        		new Runnable() {
+        			public void run() {
+        				updateStatus();
+        			}
+        		}, 0, 1, TimeUnit.SECONDS);
+        
+        Button toggleButton = (Button) findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				toggleService();
+			}
+		});
+        
+        return Service.START_STICKY;
+    }
+    
+    private RunningServiceInfo getService() {
+    	for (RunningServiceInfo s : ((ActivityManager) getSystemService(ACTIVITY_SERVICE)).getRunningServices(Integer.MAX_VALUE)) {
+    		if (s.service.getClassName().equals(DataloggerService.class.getName())) {
+    			return s;
+    		}
+    	}
+    	return null;
+    }
+    
+    private void updateStatus() {
+    	if(getService() != null) {
+    		runOnUiThread(new Runnable() {
+				public void run() {
+					statusLabel.setText(getString(R.string.status_prefix) + getString(R.string.status_running));
+				}
+    		});
+    	} else {
+    		runOnUiThread(new Runnable() {
+				public void run() {
+					statusLabel.setText(getString(R.string.status_prefix) + getString(R.string.status_not_running));
+				}
+    		});
+    	}
+    }
+    
+    private void toggleService() {
+    	RunningServiceInfo s = getService();
+    	if(s != null) {
+    		stopService(new Intent(DataloggerService.class.getName()));
+    	} else {
+    		startService(new Intent(DataloggerService.class.getName()));
+    	}
+    }
+}
---
+++ src/fi/dy/esav/Datalogger/DataloggerService.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package fi.dy.esav.Datalogger;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * @author Oppilas
+ *
+ */
+public class DataloggerService extends Service {
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	ScheduledFuture schedulerHandle;
+	ScheduledExecutorService scheduler;
+	
+	@Override
+	public void onCreate() {
+		Log.d("Datalogger", "Service created!");
+        scheduler = Executors.newScheduledThreadPool(1);
+        schedulerHandle = 
+        	scheduler.scheduleAtFixedRate(
+        		new Runnable() {
+        			public void run() {
+        				Log.d("Datalogger", "service task");
+        			}
+        		}, 0, 1, TimeUnit.SECONDS);
+        super.onCreate();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		//schedulerHandle.cancel(false);
+		scheduler.shutdown();
+		Log.d("Datalogger", "Service destroyed!");
+	}
+
+}
